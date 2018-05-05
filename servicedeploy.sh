0846b28f66c6d70276a980000b54870bda8d0a8c
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-05 16:18:35 +0800

    m

--- servicedeploy.sh
+++ servicedeploy.sh
@@ -34,11 +34,11 @@ spec:
 sleep 1
 for ((svc=1;svc<$NUM+1;svc=svc+1))
 do 
-TEMP$svc=$(kubectl get svc nodesvc1 | awk 'NR>1 {print $4}')
-while [[ $TEMP$svc = "<pending>" ]]; do
+TEMP=$(kubectl get svc nodesvc$svc | awk 'NR>1 {print $4}')
+while [[ $TEMP = "<pending>" ]]; do
 	#statements
-	TEMP$svc=$(kubectl get svc nodesvc1 | awk 'NR>1 {print $4}')
+	TEMP=$(kubectl get svc nodesvc$svc | awk 'NR>1 {print $4}')
 	
 done
-echo $TEMP$svc
+echo $TEMP
 done
