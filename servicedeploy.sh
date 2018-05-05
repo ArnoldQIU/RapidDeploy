1f54ec23bb2a441286c41c79cdbc3356bf1b5eae
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-05 15:08:36 +0800

    m

--- servicedeploy.sh
+++ servicedeploy.sh
@@ -35,8 +35,10 @@ sleep 1
 for ((svc=1;svc<$NUM+1;svc=svc+1))
 do 
 export SERVICE_IP$svc=$(kubectl get svc nodesvc$svc -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-TEMP=$(kubectl get svc nodesvc$svc | awk 'NR>1 {print $4}')
-if [${TEMP} = false]; then
-	export SERVICE_IP$svc=$(kubectl get svc nodesvc$svc -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-fi
+TEMP=$(kubectl get svc nodesvc$svc -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+while[${TEMP} = false]
+do
+export SERVICE_IP$svc=$(kubectl get svc nodesvc$svc -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+done
 done
