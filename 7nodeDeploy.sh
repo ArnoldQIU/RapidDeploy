f5ee40e37a229751f0b72e03b5dbed6370662157
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-06 14:05:06 +0800

    m

--- 7nodeDeploy.sh
+++ 7nodeDeploy.sh
@@ -16,7 +16,7 @@ do
     cp "keys/tm$i.pub" "$DDIR/tm.pub"
     cp "keys/tm$i.key" "$DDIR/tm.key"
     rm -f "$DDIR/tm.ipc"
-    CMD="constellation-node --url=https://$SERVER_IP$i:9000/ --port=9000 --workdir=$DDIR --socket=tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=https://35.184.23.185:9000/"
+    CMD="constellation-node --url=https://${SERVER_IP{i}}:9000/ --port=9000 --workdir=$DDIR --socket=tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=https://${SERVER_IP1}:9000/"
     echo "$CMD >> qdata/logs/constellation$i.log 2>&1 &"
     $CMD >> "qdata/logs/constellation$i.log" 2>&1 &
 done
