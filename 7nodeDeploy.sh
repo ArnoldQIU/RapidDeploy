d143b68954e321de992dd195f62db58e8efa9f5b
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-06 15:42:54 +0800

    m

--- 7nodeDeploy.sh
+++ 7nodeDeploy.sh
@@ -3,23 +3,23 @@
 #git clone $VOLUME
 for (( v = 1; v < $NUM_K8S+1; v++ ))
 do
-echo $SERVICE_IP$v
+echo $SERVICE_IP
 	#statements
-echo '#!/bin/bash
+echo "#!/bin/bash
 set -u
 set -e
 
 for i in 1
 do
-    DDIR="qdata/c$i"
+    DDIR='qdata/c$i'
     mkdir -p $DDIR
     mkdir -p qdata/logs
-    cp "keys/tm$i.pub" "$DDIR/tm.pub"
-    cp "keys/tm$i.key" "$DDIR/tm.key"
-    rm -f "$DDIR/tm.ipc"
-    CMD="constellation-node --url=https://'$SERVICE_IP$v':9000/ --port=9000 --workdir=$DDIR --socket=tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=https://${SERVER_IP1}:9000/"
-    echo "$CMD >> qdata/logs/constellation$i.log 2>&1 &"
-    $CMD >> "qdata/logs/constellation$i.log" 2>&1 &
+    cp 'keys/tm$i.pub' '$DDIR/tm.pub'
+    cp 'keys/tm$i.key' '$DDIR/tm.key'
+    rm -f '$DDIR/tm.ipc'
+    CMD='constellation-node --url=https://"$SERVICE_IP$v":9000/ --port=9000 --workdir=$DDIR --socket=tm.ipc --publickeys=tm.pub --privatekeys=tm.key --othernodes=https://${SERVER_IP1}:9000/'
+    echo '$CMD >> qdata/logs/constellation$i.log 2>&1 &'
+    $CMD >> 'qdata/logs/constellation$i.log' 2>&1 &
 done
 
 DOWN=true
@@ -28,9 +28,9 @@ while $DOWN; do
     DOWN=false
     for i in 1
     do
-	if [ ! -S "qdata/c$i/tm.ipc" ]; then
+	if [ ! -S 'qdata/c$i/tm.ipc' ]; then
             DOWN=true
 	fi
     done
-done' > constellation-start${v}.sh
+done" > constellation-start${v}.sh
 done
